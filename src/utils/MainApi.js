b15e1478e6d78c67fb6b6edfd817023eff664fb1
Marina Belan <belanm77@gmail.com>
2024-02-20 13:06:30 +0600

    change url

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -116,8 +116,8 @@ class MainApi {
 
 export const mainApi = new MainApi({
  // baseUrl: 'https://api.diploma.maribel.nomoredomains.club',
- baseUrl: "http://localhost:3001",
-// baseUrl: "https://movies-explorer-f863673090f5.herokuapp.com/",
+ // baseUrl: "http://localhost:3001",
+ baseUrl: "https://movies-explorer-f863673090f5.herokuapp.com/",
   headers: {
     Authorization: "",
     "Content-Type": "application/json",
