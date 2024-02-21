8432eba3c82647b10c3056095c873ef5e48083e3
Marina Belan <belanm77@gmail.com>
2024-02-21 23:49:36 +0600

    change url

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -117,7 +117,7 @@ class MainApi {
 export const mainApi = new MainApi({
  // baseUrl: 'https://api.diploma.maribel.nomoredomains.club',
 //  baseUrl: "http://localhost:3001",
- baseUrl:" https://movies-explorer-api-snowy.vercel.app",
+ baseUrl:"https://movies-explorer-api-nu.vercel.app",
   headers: {
     Authorization: "",
     "Content-Type": "application/json",
